d25da135bc38a12b96aef628c24c5fb4bb9f128c
stephen walker <stephenwalker@stephens-MacBook-Pro-2.local>
2019-03-26 17:39:52 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+
+
+var request = require("request");
+var {google} = require("googleapis");
+var key = require("./service_account.json");
+
+const jwtClient = new google.auth.JWT(
+    key.client_email,
+    null,
+    key.private_key,
+    ["https://www.googleapis.com/auth/indexing"],
+    null
+);
+
+const batch = [ "add", 
+                "your", 
+                "URLs",
+                "here"
+                ]
+
+jwtClient.authorize(function(err, tokens) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    const items = batch
+        .map(line => {
+            return {
+                'Content-Type': 'application/http',
+                'Content-ID': '',
+                body: 'POST /v3/urlNotifications:publish HTTP/1.1\n' +
+                    'Content-Type: application/json\n\n' +
+                    JSON.stringify({
+                        url: line,
+                        type: 'URL_UPDATED',
+                    }),
+            };
+        });
+    const options = {
+        url: 'https://indexing.googleapis.com/batch',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'multipart/mixed',
+        },
+        auth: { bearer: tokens.access_token },
+        multipart: items,
+    };
+    request(options, (err, resp, body) => {
+       console.log(body)
+    });
+
+});
+
+
+
